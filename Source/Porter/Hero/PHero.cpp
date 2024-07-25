4ef6c37623265d28fb653e03eec0f8f96a90091f
GangHan <rhkrrkdgks2@naver.com>
2024-07-25 18:36:25 +0900

    Fix Hero And Fix Projectile
    Fix Mesh Rotation
    Fix Bullet Spawn

--- Source/Porter/Hero/PHero.cpp
+++ Source/Porter/Hero/PHero.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Hero/PHero.h"
+
+#include "Components/CapsuleComponent.h"
 #include "Hero/PHeroStruct.h"
 #include "Hero/PHeroAIController.h"
 #include "Hero/PHeroBullet.h"
@@ -13,6 +15,9 @@ APHero::APHero()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Bullet이 Player Type을 무시하기 때문에 Hero도 Object Type을 Player로 설정
+	GetCapsuleComponent()->SetCollisionObjectType(ECC_GameTraceChannel2);
+	
 	static ConstructorHelpers::FClassFinder<UAnimInstance> AnimInstance(TEXT("/Game/Porter/Develop/Hero/ABP_PHeroAnimation.ABP_PHeroAnimation_C"));
 	if (AnimInstance.Succeeded()) 
 	{
@@ -21,7 +26,7 @@ APHero::APHero()
 
 	RangeAttackPosition = CreateDefaultSubobject<USceneComponent>(TEXT("RangeAttackPosition"));
 	RangeAttackPosition->SetupAttachment(GetMesh());
-	RangeAttackPosition->SetRelativeLocation(FVector3d(95.0f, 0.0f, 30.0f));
+	RangeAttackPosition->SetRelativeLocation(FVector3d(0, 50.0f, 120.0f));
 	
 }
 
@@ -30,16 +35,16 @@ void APHero::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// Bullet이 Player Type을 무시하기 때문에 Hero도 Object Type을 Player로 설정
+	GetCapsuleComponent()->SetCollisionObjectType(ECC_GameTraceChannel2);
 	// 스켈레탈 메시 할당
 	if (TestStruct.SkeletalMesh)
 	{
 		GetMesh()->SetSkeletalMesh(TestStruct.SkeletalMesh);
 		GetMesh()->SetRelativeLocation(FVector3d(0.0f, 0.0f, -90.0f));
 		GetMesh()->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+		GetMesh()->SetCollisionObjectType(ECC_GameTraceChannel2);
 	}
-
-	
-	
 
 	// 매개변수로 온 구조체의 정보를 영웅 멤버변수에 할당
 	Damage = TestStruct.Damage;
@@ -67,7 +72,18 @@ void APHero::BeginPlay()
 void APHero::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (bIsLookingForward)
+	{
+		LookForward();
+	}
+	if (bIsLookingTarget)
+	{
+		LookTarget();
+	}
 }
+
+
 
 void APHero::Initialize(FPHeroStruct HeroStruct)
 {
@@ -115,23 +131,41 @@ void APHero::StartAttack()
 	{
 		UE_LOG(LogTemp, Log, TEXT("Attack: %s"), *AttackTarget->GetName());
 
-		LookTarget();
-		PlayAnimMontage(AttackAnim);
+		bIsLookingTarget = true;
+		float AnimTime = PlayAnimMontage(AttackAnim);
 		RangeAttack();
-		
+		/*FOnMontageEnded MontageEndedDelegate;
+		MontageEndedDelegate.BindUObject(this, &APHero::OnAttackMontageEnded);*/
+		//FTimerHandle AnimTimerHandle;
+		//GetWorld()->GetTimerManager().SetTimer(AnimTimerHandle, this, &APHero::RangeAttack, AnimTime / 2, false);
+		//GetMesh()->GetAnimInstance()->Montage_SetEndDelegate(MontageEndedDelegate, AttackAnim);
 	}
-	
+}
+
+void APHero::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (Montage == AttackAnim)
+	{
+		// 애니메이션이 끝났을 때 불렛 생성
+		RangeAttack();
+
+		// 애니메이션이 끝난 후 다시 공격 함수 호출
+		GetWorld()->GetTimerManager().SetTimer(AttackTimerHandle, this, &APHero::StartAttack, AttackSpeed, false);
+	}
 }
 
 void APHero::RangeAttack()
 {
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	APHeroBullet* Bullet = GetWorld()->SpawnActor<APHeroBullet>(APHeroBullet::StaticClass(), RangeAttackPosition->GetComponentLocation(), FRotator(0, 0, 0), SpawnParams);
-	if (Bullet)
+	if (AttackTarget)
 	{
-		UE_LOG(LogTemp, Log, TEXT("Make Bullet"));
-		Bullet->Initialize(TestStruct.BulletMesh,TestStruct.BulletSpeed, Damage);
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		FRotator LookAtRotator = UKismetMathLibrary::FindLookAtRotation(RangeAttackPosition->GetComponentLocation(), AttackTarget->GetActorLocation());
+		APHeroBullet* Bullet = GetWorld()->SpawnActor<APHeroBullet>(APHeroBullet::StaticClass(), RangeAttackPosition->GetComponentLocation(), FRotator(LookAtRotator.Pitch - 90, LookAtRotator.Yaw, LookAtRotator.Roll), SpawnParams);
+		if (Bullet)
+		{
+			Bullet->Initialize(TestStruct.BulletMesh,TestStruct.BulletSpeed, Damage, RangeAttackPosition->GetRightVector());
+		}
 	}
 }
 
@@ -140,28 +174,50 @@ void APHero::StopAttack()
 	if (GetWorld()->GetTimerManager().IsTimerActive(AttackTimerHandle))
 	{
 		AttackTarget = nullptr;
-		UE_LOG(LogTemp, Log, TEXT("StopAttack"));
 		GetWorld()->GetTimerManager().ClearTimer(AttackTimerHandle);
-		LookForward();
+		bIsLookingForward = true;
 	}
 }
 
 void APHero::LookTarget()
 {
-	FRotator LookAtRotator = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), AttackTarget->GetActorLocation());
-	FRotator NewRotator = FRotator(GetActorRotation().Pitch, LookAtRotator.Yaw, GetActorRotation().Roll);
-	// 새로운 회전 각도를 설정
-	SetActorRotation(NewRotator);
+	if(AttackTarget)
+	{
+		FRotator CurrentRotation = GetMesh()->GetComponentRotation();
+		FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetMesh()->GetComponentLocation(), AttackTarget->GetActorLocation());
+		FRotator TargetRotation = FRotator(0, LookAtRotation.Yaw -90, 0);
+		FRotator NewRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, GetWorld()->GetDeltaSeconds(), 10.0f); // 5.0f는 회전 속도
+
+		// 새로운 회전 각도를 설정
+		GetMesh()->SetWorldRotation(NewRotation);
+
+		if (NewRotation.Equals(TargetRotation, 0.1f))
+		{
+			bIsLookingTarget = false;
+		}
+	}
+	else
+	{
+		bIsLookingTarget = false;
+	}
+	
+	
 }
 
 void APHero::LookForward()
 {
 	// 현재 회전을 천천히 목표 회전으로 보간
 	FRotator CurrentRotation = GetMesh()->GetRelativeRotation();
-	FRotator NewRotation = FMath::RInterpTo(CurrentRotation, FRotator(0.0f, -90.0f, 0.0f), GetWorld()->GetDeltaSeconds(), 5.0f); // 5.0f는 회전 속도
-
+	FRotator TargetRotation = FRotator(0.0f, -90.0f, 0.0f);
+	FRotator NewRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, GetWorld()->GetDeltaSeconds(), 5.0f); // 5.0f는 회전 속도
+	
 	// 새로운 회전 각도를 설정
-	GetMesh()->SetWorldRotation(NewRotation);
+	GetMesh()->SetRelativeRotation(NewRotation);
+
+	if (NewRotation.Equals(TargetRotation, 0.1f))
+	{
+		bIsLookingForward = false;
+	}
 }
 
 
