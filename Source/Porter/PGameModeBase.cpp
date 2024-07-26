4f727b876d716c1c2b4550328088a7cee4d8d582
YANG_MON <87008914+mon-world@users.noreply.github.com>
2024-07-26 17:33:35 +0900

    gm이 아닌 gi로 해야함

--- Source/Porter/PGameModeBase.cpp
+++ Source/Porter/PGameModeBase.cpp
@@ -2,7 +2,67 @@
 
 
 #include "PGameModeBase.h"
+#include "PPlayer.h"
+#include "PHero.h"
+#include "PGameStateBase.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 APGameModeBase::APGameModeBase()
 {
+	// 생성자에서 해야 하나
+	// PPlayer = Cast<APPlayer>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+	
 }
+
+void APGameModeBase::SpawnPort(int32 PortTpye, FVector CurrentLocation, FVector ForwardV, FVector RightV)
+{
+	PPlayer = Cast<APPlayer>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+	//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::FromInt(1000));
+	
+	APGameStateBase* PGameState = GetWorld()->GetGameState<APGameStateBase>();
+	
+	if(PGameState) 		
+	{
+		int32 PortNum = PGameState->CheckPortNum();
+		//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::FromInt(PortNum));
+		
+		if (PortNum < PGameState->MaxPortNum)
+		{
+			FVector RelativeOffset = ForwardV*PGameState->OffsetArray[PortNum].X + RightV*PGameState->OffsetArray[PortNum].Y + FVector(0,0,PGameState->OffsetArray[PortNum].Z);
+			FVector SpawnLocation = CurrentLocation + RelativeOffset;
+
+			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::FromInt(RelativeOffset.X));
+			
+			AActor* Port = GetWorld()->SpawnActor<AActor>(PortClassArray[PortTpye], SpawnLocation, PPlayer->GetActorRotation());
+
+			if (Port)
+			{
+				PGameState->PortArray[PortNum] = Port;
+				Port->AttachToComponent(PPlayer->GetRootComponent(), FAttachmentTransformRules::KeepWorldTransform);
+			}
+			PPlayer->SpringArm->TargetArmLength = 400 + PGameState->PortFloorArray[PortNum+1]*PPlayer->AddCameraLength;
+			Port->SetActorEnableCollision(false);
+			
+		}
+		
+	}
+	
+}
+
+void APGameModeBase::DestroyPort(int32 Value)
+{
+}
+
+void APGameModeBase::SpawnHero(int32 HeroType)
+{
+}
+
+void APGameModeBase::DestroyHero(int32 Value)
+{
+}
+
+/*
+ 플레이어를 알아야 붙임
+ hero, port을 알아야 소환
+ */
--- Source/Porter/PGameStateBase.h
+++ Source/Porter/PGameStateBase.h
@@ -17,12 +17,37 @@ class PORTER_API APGameStateBase : public AGameStateBase
 public:
 	APGameStateBase();
 
+	// 정말 주의해서 바꿀 것 : 모든 배열의 사이즈
+	int32 MaximumArraySize = 15;
+
+	// 플레이어
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Options")
+	float mouseSpeed = 30;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Options")
+	float AddCameraLength = 300;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	int32 MaxHp = 5;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	float MaxStamina = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	float DecreaseStamina = 33; // 속력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	float IncreaseStamina = 20; // 속력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	float ZeroToHundredIncreaseStamina = 10; // 속력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	float PlayerWalkSpeed = 400;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Player")
+	float PlayerBoostSpeed = 1000;
+	
 	// 공통
 	UPROPERTY()
 	TArray<FVector> OffsetArray;
+	float OffsetX = -100;
+
 	
 	// 지게 관련
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Port")
 	TArray<AActor*> PortArray;
 	UPROPERTY()
 	TArray<int32> PortFloorArray;
@@ -32,13 +57,22 @@ public:
 	float PortWidth = 100;
 	UPROPERTY()
 	float PortWeight = 0.2;
+	UFUNCTION()
+	int32 CheckPortNum();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Port")
+	int32 MaxPortNum = 15;
 
 	// 영웅 관련
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats|Hero")
 	TArray<class APHero*> HeroArray;
+	UPROPERTY()
+	TArray<int32> HeroIndexArray;
 	UPROPERTY()
 	float HeroWeight = 0.8;
 	UPROPERTY()
 	float HeroOffset = 50;
+	UFUNCTION()
+	int32 CheckHeroNum();
+
 	
 };
